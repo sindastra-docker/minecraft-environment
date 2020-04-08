8ad2f57a73ade1c387d86db723d312c9370abffe
Sindastra Docker <62795136+sindastra-docker@users.noreply.github.com>
2020-04-09 00:10:40 +0200

    Create script.sh

---
+++ hub/script.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+echo "You should read more carefully!"
+echo "You were supposed to replace script.sh with either minecraft.sh or spigot.sh depending on what server version you want to run!"
